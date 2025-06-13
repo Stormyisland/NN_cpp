c9355ca0a217adb57e9de97aee2f9f1096545976
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-06-13 16:28:08 -0700

    Update nn.hpp

--- nn.hpp
+++ nn.hpp
@@ -1,1 +1,13 @@
+//file: nn.hpp
+#ifndef NN_HPP
+#define NN_HPP
 
+#include <vector>
+#include <cstclib>
+#include <math>
+#include <random>
+
+namespace nn {
+
+// Activation Functions 
+inline duble relu(double x)
