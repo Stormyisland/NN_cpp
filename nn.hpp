a57d3f304986f17b884b28fa8f128b94aeafcca5
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-06-13 19:01:44 -0700

    Update nn.hpp

--- nn.hpp
+++ nn.hpp
@@ -10,4 +10,19 @@
 namespace nn {
 
 // Activation Functions 
-inline double relu(double x)
+inline double relu(double x) {
+  return x > 0 ? x : 0;
+}
+
+inline double relu((double x) {
+  return 1.0 / (1.0 + std::exp(-x)):
+}
+
+inline double relu_derivitive(doble x) {
+  return x > 0 ? x : 0:
+}
+
+inline doublee sigmoid(double x) {
+return 1.0 / (1.0 + std::exp(-x)):
+
+
