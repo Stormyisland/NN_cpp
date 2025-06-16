131316e06bb981cb2a519e4ce8b54509e5dc1849
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-06-16 14:21:06 -0700

    Update nn.cpp

--- nn.cpp
+++ nn.cpp
@@ -4,20 +4,19 @@
 int main() {
     nn::NerualNetwork net({2, 3, 1});
 
-    std::vector<std::vector<duble>> X = {
+    std::vector<std::vector<double>> X = {
         {0, 0}, {0, 1}, {1. 0}, {1, 1} 
     };
 
-    std::vector<std::veeeector<double>> y = {
+    std::vector<std::vector<double>> y = {
         {0}, {1}, {1}, {0} // XOR function
     };
 
     net.train(X, y, 10000, 0.1);
     
     for (cont auto& input : X) {
-        auto output : X) {
         auto output = net.predict(input);
-        std::cout << input[0] << ","<<input[1] << "=>" << output[0] ,, std::end1:
+        std::cout << input[0] << " , " << input[1] << " => " << output[0] << std::end1:
     }
     return 0;
 }
